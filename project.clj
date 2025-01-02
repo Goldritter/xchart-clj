e0140a64f9d594f434ddff3519631c3751f469c7
Goldritter <marcus.goldritter.lindner@gmail.com>
2025-01-02 18:12:55 +0100

    Licence update

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject xchart-clj "0.1.0-SNAPSHOT"
+(defproject com.github.goldritter/xchart-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"
+            :comment "MIT License"
+            :year 2025
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.knowm.xchart/xchart "3.8.8"]
                  [commons-io/commons-io "2.18.0"]]
+  :plugins [[lein-license "1.0.0"]]
   :main ^:skip-aot xchart-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
